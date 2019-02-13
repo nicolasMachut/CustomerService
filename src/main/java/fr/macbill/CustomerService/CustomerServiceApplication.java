da1d20bfca5fff05e76d658f0fea6ccdd4afc808
nicolasmachut <machut.nicolas@gmail.com>
2019-02-12 19:40:43 -0500

    add gateway and discovery

--- src/main/java/fr/macbill/CustomerService/CustomerServiceApplication.java
+++ src/main/java/fr/macbill/CustomerService/CustomerServiceApplication.java
@@ -2,8 +2,10 @@ package fr.macbill.CustomerService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
+@EnableEurekaClient
 @EnableReactiveMongoRepositories
 @SpringBootApplication
 public class CustomerServiceApplication {
--- src/main/java/fr/macbill/CustomerService/services/CustomerService.java
+++ src/main/java/fr/macbill/CustomerService/services/CustomerService.java
@@ -1,25 +1,11 @@
 package fr.macbill.CustomerService.services;
 
 import fr.macbill.CustomerService.documents.Customer;
-import fr.macbill.CustomerService.repositories.CustomerRepository;
-import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@Service
-public class CustomerService {
+public interface CustomerService {
 
-    private final CustomerRepository customerRepository;
-
-    public CustomerService(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
-
-    public Flux<Customer> findAll () {
-        return this.customerRepository.findAll();
-    }
-
-    public Mono<Customer> save (Customer customer) {
-        return this.customerRepository.save(customer);
-    }
+    Flux<Customer> findAll ();
+    Mono<Customer> save (Customer customer);
 }
